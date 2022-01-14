ce75ea7aba11c631dcb5c18475d0af9838e7b310
Srishti-55 <srishtichanda14@gmail.com>
2022-01-14 22:59:00 +0530

    edited app.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import {AuthProvider} from './Context/AuthContext'
 
 function App() {
      return (
-     <Router>
-        <Route path="/login" element={<Login/>}/>
+     <BrowserRouter>
+     <AuthProvider>
+     <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
 
-         
-       
-     
-     </Router>
+     </AuthProvider>
+    </BrowserRouter>
 
 
       
