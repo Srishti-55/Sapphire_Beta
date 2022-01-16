59d4d644591f40c6f636ec96fa530d49b86f54ac
Srishti-55 <srishtichanda14@gmail.com>
2022-01-16 20:33:34 +0530

    unresponsive route fixed

---
+++ src/Components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react'
+import { AuthContext} from '../Context/AuthContext';
+import {Route} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+
+function PrivateRoute({children}){
+
+    const {user} =useContext(AuthContext);
+    console.log(user);
+    
+            return user?children : <Navigate to="login"/>
+}
+
+export default PrivateRoute
