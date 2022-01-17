9b5175107a11e686919c8dea5873dd784890e7e9
Srishti-55 <srishtichanda14@gmail.com>
2022-01-17 23:25:38 +0530

    feed updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
         <Routes>
                <Route path="/login" element={<Login/>}/>
                <Route path="/signup" element={<Signup/>}/>
-              <Route path="/" element={<PrivateRoute><Feed/></PrivateRoute>}/> 
+              {/* <Route path="/" element={<PrivateRoute><Feed/></PrivateRoute>}/>*/} 
+              <Route path="/" element={<Feed/>}/>
             </Routes>
        </AuthProvider>
             
