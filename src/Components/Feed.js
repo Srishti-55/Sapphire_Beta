c0ad93cfaf2ec0d52888ce59b90f43b5739b0c82
Srishti-55 <srishtichanda14@gmail.com>
2022-02-12 23:20:57 +0530

    bug fixed user.uid

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,12 +2,15 @@ import React, {useContext,useEffect,useState} from 'react'
 import {AuthContext} from '../Context/AuthContext'
 import {database} from '../firebase'
 import UploadFile from './UploadFile'
+import Posts from './Posts'
 function Feed()
 {
+    
     const {user , logout}=useContext (AuthContext)
+    console.log(user)
     const [userData, setUserData]=useState('')
     useEffect(()=>{
-        const unsub=database.users.doc(user.uid).onSnapshot((snapshot)=>{
+        const unsub=database.users.doc(user?.uid).onSnapshot((snapshot)=>{
           setUserData(snapshot.data())
 
         })
@@ -23,6 +26,7 @@ function Feed()
             </div>
             
             <UploadFile user={userData}/>
+            <Posts userData={userData}/>
            
  
 
